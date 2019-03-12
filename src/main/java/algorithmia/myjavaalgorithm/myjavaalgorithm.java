ee75c6687ab67eb0c422f00231a934fa02e1546d
ons <adrian.harris@ons.gov.uk>
2019-03-12 13:21:34 +0000

    Algorithmia initial commit

---
+++ src/main/java/algorithmia/myjavaalgorithm/myjavaalgorithm.java
@@ -0,0 +1,13 @@
+package algorithmia.myjavaalgorithm;
+
+import com.algorithmia.*;
+import com.algorithmia.algo.*;
+import com.algorithmia.data.*;
+import com.google.gson.*;
+import java.util.*;
+
+public class myjavaalgorithm {
+    public String apply(String s) throws Exception {
+        return "Hello " + s;
+    }
+}
